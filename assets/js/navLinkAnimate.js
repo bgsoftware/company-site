6aae24624713323caf9bdb9451cd15ede62a6a38
bwuphan <bwuphan@gmail.com>
2019-04-12 13:30:43 -0700

    Add navbar animation

---
+++ assets/js/navLinkAnimate.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $(".nav-link").on('click', function(event) {
+    const numMs = 800; // Number of ms it takes to scroll to the specified area.
+
+    // Make sure this.hash has a value before overriding default behavior.
+    if (this.hash !== "") {
+      // Prevent default click behavior.
+      event.preventDefault();
+
+      // Store hash.
+      const hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, numMs, function() {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
+  });
+
+
+});
