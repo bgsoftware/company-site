6bdc3e2bcf98c9da3d9c45b212f988c79cb1d87c
bwuphan <bwuphan@gmail.com>
2019-04-17 15:00:41 -0700

    Change navigation-bar to navigation

--- assets/js/navLinkAnimate.js
+++ assets/js/navLinkAnimate.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
   $(".nav-link").on('click', function(event) {
-    const numMs = 800; // Number of ms it takes to scroll to the specified area.
+    const numMs = 800; /* Number of ms it takes to scroll to the specified area. */
 
-    // Make sure this.hash has a value before overriding default behavior.
+    /* Make sure this.hash has a value before overriding default behavior. */
     if (this.hash !== "") {
-      // Prevent default click behavior.
+      /* Prevent default click behavior. */
       event.preventDefault();
 
-      // Store hash.
+      /* Store hash. */
       const hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
+      /* Using jQuery's animate() method to add smooth page scroll. */
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, numMs, function() {
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
+        /* Add hash (#) to URL when done scrolling (default click behavior). */
         window.location.hash = hash;
       });
     }
