8cdf35ac36caf33850911f47ac8cca1cbbdae270
bwuphan <bwuphan@gmail.com>
2019-04-26 17:28:00 -0700

    Rename assets/js/navLinkAnimate.js -> assets/js/scrollAnimate.js

--- assets/js/scrollAnimate.js
+++ assets/js/scrollAnimate.js
@@ -1,7 +1,7 @@
 (function(){
-  $(".nav-link").on('click', function(event) {
-    const numMs = 800; /* Number of ms it takes to scroll to the specified area. */
+  const numMs = 800; /* Number of ms it takes to scroll to the specified area. */
 
+  $(".nav-link").on('click', function(event) {
     /* Make sure this.hash has a value before overriding default behavior. */
     if (this.hash !== "") {
       /* Prevent default click behavior. */
@@ -14,4 +14,10 @@
       $('html, body').animate({ scrollTop: $(hash).offset().top }, numMs);
     }
   });
+
+  $("#scroll-up-button").on('click', function(event) {
+    event.preventDefault();
+
+    $('html, body').animate({ scrollTop: 0 }, numMs);
+  })
 })();
