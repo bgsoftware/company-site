6713318ce50e66c229e8cfce35fbfa0d8f3b5b1a
bwuphan <bwuphan@gmail.com>
2019-06-03 15:11:25 -0700

    Add assets/js/navOverlay.js

---
+++ assets/js/navOverlay.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("navigation-overlay").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("navigation-overlay").style.width = "0%";
+}
